a4bded3213b6c01cabaa1fd28c5cb528af573e31
Hassanibrar632 <118011584+Hassanibrar632@users.noreply.github.com>
2022-11-29 23:15:26 +0500

    NODES

---
+++ Node.h
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+//Set items, shops and inventory of the shop.
+
+class Node{
+public:
+	string Name;
+	Node* Next;
+	
+	Node(){
+		Next = NULL;
+	}
+	
+};
